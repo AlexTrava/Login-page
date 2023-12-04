a85010985c47f08d870e205ac575d4001e67da75
AlexTrava <106436234+AlexTrava@users.noreply.github.com>
2023-12-04 12:50:33 +0000

    add loginForm component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
+
+import LeftBar from "./components/LeftBar/LoginForm";
 function App() {
   return (
-    <AppShell
-      navbar={
-        <Navbar width={{ base: 300 }} height="100vh">
-          <Navbar.Section>Assets/Hosts</Navbar.Section>
-          <Navbar.Section>Software</Navbar.Section>
-          <Navbar.Section>Configurations</Navbar.Section>
-        </Navbar>
-      }>
-      {/* Your application here */}
-    </AppShell>
+    <LeftBar />
   );
 }
 
