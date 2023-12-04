d12237560a49d9b7aad9c55fd02028cae2cbb441
A1eX <106436234+AlexTrava@users.noreply.github.com>
2023-12-04 07:50:40 +0000

    start project dev branch

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
 import './index.css';
 
+import { MantineProvider } from '@mantine/core';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App.tsx';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider
+      theme={{
+        colorScheme: 'dark'
+      }}>
+      <App />
+    </MantineProvider>
   </React.StrictMode>
 );
