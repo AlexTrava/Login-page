99bc3072ade5493cd9aa89777c35d90ce74cd21d
AlexTrava <106436234+AlexTrava@users.noreply.github.com>
2023-12-05 18:22:31 +0000

    fix LanguageButton

--- src/components/InputLanguage/InputLanguage.tsx
+++ src/components/InputLanguage/InputLanguage.tsx
@@ -1,9 +1,22 @@
 import { NativeSelect, Image } from '@mantine/core';
 import language from '../../../public/language.svg';
+import { useState } from 'react';
 
 
 const InputLanguage = () => {
-  return <NativeSelect mt="md" data={['RU', 'EN']} leftSection={<Image h={20} w={20} src={language} />}/>;
+
+  const [value, setValue] = useState('');
+  console.log(value, 'its state')
+
+  return (
+    <NativeSelect
+      value={value}
+      onChange={(event) => setValue(event.currentTarget.value)}
+      data={['RU', 'EN']}
+      mt="md" 
+      leftSection={<Image h={20} w={20} src={language} />}
+    />
+  );
 };
 
 export default InputLanguage;
